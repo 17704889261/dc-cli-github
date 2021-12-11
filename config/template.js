0593c1451352ec9f0685ec4b48187a188b18b1d5
张庭玮 <17704889261@163.com>
2021-12-12 00:32:32 +0800

    fix(template): 修复了模版代码地址错误的问题

--- config/template.js
+++ config/template.js
@@ -1,7 +1,7 @@
 /**
  *  模板配置文件
  */
- const baseUrl = 'direct:https://code.bonc.com.cn/dc-team/templetes/';
+ const baseUrl = "direct:https://github.com/17704889261/";
  const templatesConfig = {
      default: `${baseUrl}vue2-ts-webpack.git`,
      'vue2-ts-webpack': `${baseUrl}vue2-ts-webpack.git`,
